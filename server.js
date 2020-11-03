8afc625b92263c0279fb7cb3475004e4766a8621
Amy Keneson <amy.l.keneson@icloud.com>
2020-11-03 16:09:46 -0600

    server.js modified to add ports and links to local host

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -25,4 +25,5 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  console.log("http://localhost:3000/")
 });
